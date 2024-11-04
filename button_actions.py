9774ae46387cbf6522521fbca62aec0ec623cc50
xxlong727 <3096395473@qq.com>
2024-11-04 22:32:54 +0800

    修改ui继承QmainWindows,使用Qstackwidget优化切换界面方式

--- button_actions.py
+++ button_actions.py
@@ -5,12 +5,12 @@ from PyQt5.QtWidgets import QTableWidget,QSpacerItem, QSizePolicy
 from PyQt5.QtWidgets import QTableWidgetItem,QWhatsThis,QMessageBox
 from PyQt5.QtCore import Qt,QEvent
 #选择模型类
-class choose_btn(QDialog):
+class choose_page(QWidget):
     def __init__(self, parent =None):
         super().__init__(parent)
 
         self.setWindowTitle("选择模型")
-        self.resize(700,250)
+        self.resize(700,700)
         
         layout = QVBoxLayout(self)
 
@@ -19,11 +19,11 @@ class choose_btn(QDialog):
 
         layout.addStretch()
     #重写QDialog类的event方法，当点击(?)时触发提示
-    def event(self, event):
-        if event.type()==QEvent.EnterWhatsThisMode:
-            QWhatsThis.leaveWhatsThisMode()
-            QMessageBox.information(self, "Help", "选择一个模型")
-        return QDialog.event(self,event)
+    # def event(self, event):
+    #     if event.type()==QEvent.EnterWhatsThisMode:
+    #         QWhatsThis.leaveWhatsThisMode()
+    #         QMessageBox.information(self, "Help", "选择一个模型")
+    #     return QDialog.event(self,event)
 
 #搜索栏
     def init_search(self):
@@ -47,9 +47,9 @@ class choose_btn(QDialog):
         table_layout = QHBoxLayout()
         
         
-        table_widget = QTableWidget(2,2)
-        table_widget.setColumnWidth(1,600)
-
+        table_widget = QTableWidget(10,2)
+        table_widget.setColumnWidth(1,700)
+        table_widget.setFixedHeight(650)
         action_layout = QVBoxLayout()#左侧按钮竖直布局
         btn_add = QPushButton("确认")
         action_layout.addWidget(btn_add)
@@ -58,5 +58,45 @@ class choose_btn(QDialog):
         table_layout.addWidget(table_widget)
         table_layout.addLayout(action_layout)
 
-       
         return table_layout
+
+class home_page(QWidget):
+    def __init__(self, parent =None):
+        super().__init__(parent)
+        self.resize(700,700)
+        home_layout = QVBoxLayout(self)#总空间
+
+        #顶部控件
+        header_layout = QHBoxLayout()
+
+        btn_1 = QPushButton("1")
+        btn_1.setFixedSize(50,100)
+        btn_2 = QPushButton("2")
+        btn_3 = QPushButton("3")
+        btn_4 = QPushButton("4")
+
+        header_layout.addWidget(btn_1)
+        header_layout.addWidget(btn_2)
+        header_layout.addWidget(btn_3)
+        header_layout.addWidget(btn_4)
+
+        #中间文本显示
+        text_area = QTextEdit()
+        text_area.setReadOnly(True)
+        
+        #底部对话
+        chat_layout = QHBoxLayout()
+
+        chat_area = QTextEdit()
+        chat_area.setBaseSize(800,100)
+        chat_area.setMaximumHeight(150)
+        chat_layout.addWidget(chat_area)
+
+
+        btn_send = QPushButton("发送")
+        btn_send.setFixedSize(175,75)
+        chat_layout.addWidget(btn_send)
+
+        home_layout.addLayout(header_layout)
+        home_layout.addWidget(text_area)
+        home_layout.addLayout(chat_layout)
--- main.py
+++ main.py
@@ -1,171 +1,99 @@
 import sys
-import os
-from PyQt5.QtWidgets import QApplication,QWidget,QFrame
-from PyQt5.QtWidgets import QHBoxLayout,QVBoxLayout
-from PyQt5.QtWidgets import QPushButton
-from PyQt5.QtWidgets import QLineEdit,QTextEdit
-from PyQt5.QtWidgets import QTableWidget,QSpacerItem, QSizePolicy
-from PyQt5.QtWidgets import QTableWidgetItem
-from PyQt5.QtWidgets import QLabel, QSplitter
-from PyQt5.QtWidgets import QMessageBox
-from PyQt5.QtWidgets import QMenu,QAction
-from PyQt5.QtCore import Qt
-
-BASE_PATH =  os.path.dirname(os.path.abspath(__file__))
-
-class built_window(QWidget):
-
+from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget, QPushButton, QBoxLayout, QWidget, QLabel
+from PyQt5.QtWidgets import QHBoxLayout,QVBoxLayout,QFrame
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.initUI()
+
+    def initUI(self):
         self.setWindowTitle("gui")
         self.resize(1200,700)
         self.setMinimumSize(500,500)
-       # self.
-       # print(BASE_PATH)
-        layout = QHBoxLayout()
 
-        splitter = QSplitter(Qt.Horizontal)
+        # 创建 QStackedWidget
+        self.stacked_widget = QStackedWidget(self)
 
-        splitter.addWidget(self.init_left())
-      #  layout.addStretch()
-        splitter.setMinimumSize(0, 100)
+        # 创建页面
+        #待重写
+        self.home_page = self.homepage("首页")
+        self.choose_page = self.choosepage("选择模型")
+        self.download_page = self.createPage("下载")
+        self.theme_page = self.createPage("主题")
 
-        splitter.addWidget(self.init_side())
-        splitter.setChildrenCollapsible(False)#不能通过拖动分隔条来压缩这些子部件
-        layout.addWidget(splitter)
-       # layout.addStretch()
+        # 将页面添加到 QStacked_widget栈页面
+        self.stacked_widget.addWidget(self.home_page)
+        self.stacked_widget.addWidget(self.choose_page)
+        self.stacked_widget.addWidget(self.download_page)
+        self.stacked_widget.addWidget(self.theme_page)
 
+        # 创建左侧按钮栏
+       # self.left_Panel = QWidget()   self.left_Panel
+        self.left_layout = QVBoxLayout()
+    
+        btn_start = QPushButton("首页")
+        btn_start.setFixedHeight(100)
+        btn_start.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
+       # btn_start.setMinimumWidth(150)
+        btn_choose = QPushButton("选择模型")
+        btn_choose.setFixedHeight(100)
+        btn_choose.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(1))
+       # btn_choose.clicked.connect(self.choose_model)#绑定事件
+    
+        btn_download = QPushButton("下载模型")
+        btn_download.setFixedHeight(100)
+        btn_download.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(2))
 
-        self.setLayout(layout)#将组件添加到窗口
+        btn_theme = QPushButton("切换主题")
+        btn_theme.setFixedHeight(100)
+        btn_theme.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(3))
 
+        self.left_layout.addWidget(btn_start)
+        self.left_layout.addWidget(btn_choose)
+        self.left_layout.addWidget(btn_download)
+        self.left_layout.addWidget(btn_theme)
+        self.left_layout.addStretch()
 
-    #左边视图
-    def init_left(self):
-        #frame 方框
+        # 设置主窗口的布局
+        self.main_layout = QHBoxLayout()#水平的
+
         frame1 = QFrame(self)
         frame1.setFrameShape(QFrame.Box)
         frame1.setLineWidth(1)
         frame1.setBaseSize(200,900)
-      #  frame1.setMinimumWidth(100)
-        left_layout = QVBoxLayout(frame1)
+        frame1.setFixedWidth(200)
+        frame1.setLayout(self.left_layout)
 
-        btn_start = QPushButton("初始化")
-        btn_start.setFixedHeight(100)
-       # btn_start.setMinimumWidth(150)
-        btn_choose = QPushButton("选择模型")
-        btn_choose.setFixedHeight(100)
-        btn_choose.clicked.connect(self.choose_model)#绑定事件
+        self.main_layout.addWidget(frame1)
+        self.main_layout.addWidget(self.stacked_widget)
+
+        # 设置中央窗口和布局
+        central_Widget = QWidget()
+        central_Widget.setLayout(self.main_layout)
+        self.setCentralWidget(central_Widget)
+
+    def createPage(self, title):
+        page = QWidget()
+        layout = QVBoxLayout(page)
+        label = QLabel(title)
+        layout.addWidget(label)
+        return page
+
+
+    def homepage(self,str):#选择模型功能
+
+        from button_actions import home_page
+        win_home = home_page(self)
+        return win_home
     
-        btn_download = QPushButton("下载模型")
-        btn_download.setFixedHeight(100)
+    def choosepage(self,str):#选择模型功能
 
-        btn_theme = QPushButton("切换主题")
-        btn_theme.setFixedHeight(100)
+            from button_actions import choose_page
+            win_choose = choose_page(self)
+            return win_choose
 
-        left_layout.addWidget(btn_start)
-        left_layout.addWidget(btn_choose)
-        left_layout.addWidget(btn_download)
-        left_layout.addWidget(btn_theme)
-        left_layout.addStretch()
-        #空间占用项目
-       # item_space = QSpacerItem(100,100)
-
-       # left_layout.addItem(item_space)
-        return frame1
-  
-    def choose_model(self):#选择模型功能
-
-        from button_actions import choose_btn
-        win_choose = choose_btn(self)
-        win_choose.exec_()
-
-
-
-#中部视图
-    def init_side(self):
-        #第一层方框
-        frame = QFrame(self)
-        frame.setFrameShape(QFrame.Box)
-        frame.setLineWidth(1)
-        frame.setBaseSize(950,900)
-       
-        side_layout = QVBoxLayout(frame)#frame方框遵守竖直排布
-
-        #第二层方框1  header栏
-        self.side_header_frame(frame)
-        #方框2 显示文本栏
-        self.side_text_frame(frame)
-        #方框3 发送栏
-        self.side_chat_frame(frame)
-       # side_layout.addStretch()
-       # item_space = QSpacerItem(700,300)
-       # side_layout.addItem(item_space)
-        return frame
-
-#创建二级顶部方框功能键                                                                                                                                                                                                                                                                  
-    def side_header_frame(self, frame):
-        frame_header = QFrame(frame)
-        frame_header.setFrameShape(QFrame.Box)
-        frame_header.setLineWidth(0)
-        frame_header.setBaseSize(900,100)
-
-        side_layout = QHBoxLayout(frame_header) #继承顶部方框
-
-        btn_1 = QPushButton("1")
-        btn_1.setFixedSize(50,100)
-        btn_2 = QPushButton("2")
-        btn_3 = QPushButton("3")
-        btn_4 = QPushButton("4")
-
-        side_layout.addWidget(btn_1)
-        side_layout.addWidget(btn_2)
-        side_layout.addWidget(btn_3)
-        side_layout.addWidget(btn_4)
-
-        frame.layout().addWidget(frame_header)
-#创建文本框
-    def side_text_frame(self,frame):
-
-        text_frame = QFrame(frame)
-        text_frame.setLineWidth(0)
-        text_frame.setFrameShape(QFrame.Box)
-        text_frame.setBaseSize(900,650)
-       
-        text_layout = QHBoxLayout(text_frame)
-
-        text_area = QTextEdit()
-        text_area.setReadOnly(True)
-        text_layout.addWidget(text_area)
-        frame.layout().addWidget(text_frame)#.layout()获取frame当前的布局，再添加
-#创建对话框
-    def side_chat_frame(self,frame):
-        chat_frame = QFrame(frame)
-
-        chat_frame.setLineWidth(0)
-        chat_frame.setFrameShape(QFrame.Box)
-        chat_frame.setBaseSize(900,200)
-        chat_frame.setFixedHeight(150)
-
-        chat_layout = QHBoxLayout(chat_frame)
-        #对话栏
-        chat_area = QTextEdit()
-        chat_area.setBaseSize(800,100)
-       # chat_area.setMinimumHeight(100)
-        chat_layout.addWidget(chat_area)
-
-
-        btn_send = QPushButton("发送")
-        btn_send.setFixedSize(175,75)
-        chat_layout.addWidget(btn_send)
-        frame.layout().addWidget(chat_frame)
-
-
-
-if __name__ == '__main__':
-    #创建QApplication类的实例
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    w = built_window()#实例化对象
-#显示窗口
-    w.show()
-# 确保所有事件循环和线程都被正确清理和关闭
+    mainWin = MainWindow()
+    mainWin.show()
     sys.exit(app.exec_())
